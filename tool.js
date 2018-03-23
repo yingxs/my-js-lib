6c595d8402799722e5bad25a81d38c2752504972
王昊 <yingxs888@126.com>
2018-03-23 21:48:48 +0800

    fix (tool.js):通过传统事件绑定模拟现代事件绑定，修复IE浏览器的一些bug
    1.IE下可以传递目标对象并删除事件
    2.IE下可以按顺序执行绑定函数
    3.把IE的事件方法与W3C的事件方法做匹配

--- tool.js
+++ tool.js
@@ -7,15 +7,71 @@
  * @param fn
  */
 
+//为每个事件分配一个计数器
+addEvent.ID = 1;
 //跨浏览器事件绑定
 function addEvent(obj,type,fn){
 	if(typeof obj.addEventListener != 'undefined'){
 		obj.addEventListener(type,fn,false);
-	}else if(typeof obj.attachEvent != 'undefined'){
-		obj.attachEvent('on'+type,function(){
-			fn.call(obj,window.event);       //兼容IE无法传递this为目标对象的问题
-		});
+	}else {
+		//创建一个存放事件的哈希表(散列表)
+		if(!obj.events) obj.events = {};
+
+		//第一次绑定事件时执行
+		if(!obj.events[type]){
+			//创建一个存放事件处理函数的数组
+			obj.events[type]=[];
+
+			//把第一次的事件处理函数先储存到第一个位置上
+			obj.events[type][0] = fn;
+		}else{
+			//同一个注册函数进行屏蔽
+			if(addEvent.equal(obj.events[type],fn)) return false;
+			//从第二次事件绑定开始我们用事件计数器来存储
+			obj.events[type][addEvent.ID++] = fn;
+		}
+
+
+		//绑定事件处理函数
+		obj['on'+type] = addEvent.exec;
+
+
 	}
+}
+
+
+addEvent.exec = function(){
+	var e = event || window.event;
+	var es = this.events[e.type];
+	for(var i in es){
+		es[i].call(this,e);
+	}
+};
+
+
+//同一个注册函数进行屏蔽
+addEvent.equal = function(es,fn){
+	for(var i in es){
+		if(es[i] == fn)return true;
+	}
+	return false;
+};
+
+//把IE常用的Event对象配对到W3C中去
+addEvent.fixEvent = function(event){
+	event.preventDefault = addEvent.fixEvent.preventDefault;
+	event.stopPropagation =addEvent.fixEvent.stopPropagation;
+	return event;
+};
+
+//IE阻止默认行为
+addEvent.fixEvent.preventDefault = function(){
+	this.returnValue = false;
+};
+
+//IE取消冒泡
+addEvent.fixEvent.stopPropagation = function(){
+	this.cancelBubble = true;
 }
 
 //跨浏览器删除事件
@@ -23,7 +79,11 @@ function removeEvent(obj,type,fn){
 	if(typeof obj.removeEventListener != 'undefined'){
 		obj.removeEventListener(type,fn,false);
 	}else if(typeof obj.detachEvent != 'undefined'){
-		obj.detachEvent('on'+type,fn);
+		for(var i in obj.events[type]){
+			if(obj.events[type][i] == fn){
+				delete obj.events[type][i];
+			}
+		}
 	}
 }
 
