adeebbef5da6662e23ac1d8c77176bbaa75104af
王昊 <yingxs888@126.com>
2018-03-25 23:17:26 +0800

    feat (base.js):优化Base构造函数，实现模拟css选择期选择元素，优化库方法

--- base.js
+++ base.js
@@ -31,48 +31,152 @@ var $ = function (_this){
 };
 
 //基础类库对象
-function Base(_this){
+function Base(args){
 	//创建一个数组，来保存获取的结点和结点数组
 	this.elements=[];
-	if(_this != undefined) { //_this 是一个对象，区别于typeof返回的带单引号字符串
-		this.elements[0] = _this;
+
+	if(typeof args == 'string'){
+		//CSS模拟
+		if(args.indexOf(' ') != -1){
+			var css = args.split(' ');             //把结点拆开分别保存到数组
+			var childElements = [];                     //存放临时结点对象的数组解决被覆盖问题
+			var node = [];                              //用来存放父节点
+			for(var i=0; i < css.length;i++){
+				if(node.length == 0 )
+					node.push(document);                //如果没有父节点，就将document放入
+				switch (css[i].charAt(0)){
+					case '#':
+						childElements = [];             //清理掉临时节点，以便父节点失效，子节点有效
+						childElements.push(this.getId(css[i].substring(1)));
+						node = childElements;           //保存父节点，因为childElement要清理，所以需要创建node数组
+						alert(css[i].substring(1)+"\r\nchildElements:"+childElements+"\r\n"+"node:"+node);
+						break;
+					case '.':
+						childElements = [];
+						for(var j=0 ;j<node.length;j++ ){
+							var temps = this.getClass(css[i].substring(1),node[j]);
+							for(var k = 0; k < temps.length;k++){
+								childElements.push(temps[k]);
+							}
+						}
+						node = childElements;
+						alert(css[i]+"\r\nchildElements:"+childElements+"\r\n"+"node:"+node);
+						break;
+					default :
+						childElements = [];
+						for(var j=0 ;j<node.length;j++ ){
+							var temps = this.getTagName(css[i],node[j]);
+							for(var k = 0; k < temps.length;k++){
+								childElements.push(temps[k]);
+							}
+						}
+						node = childElements;
+						alert(css[i]+"\r\nchildElements:"+childElements+"\r\n"+"node:"+node);
+				}
+			}
+			this.elements = childElements;
+		}else{
+			//find模拟
+			switch (args.charAt(0)){
+				case '#':
+					this.elements.push( this.getId(args.substring(1)));
+					break;
+				case '.':
+					this.elements =  this.getClass(args.substring(1));
+					break;
+				default :
+					this.elements =  this.getTagName(args);
+			}
+		}
+
+
+	}else if(typeof args == 'object'){
+		if(args != undefined) { //_this 是一个对象，区别于typeof返回的带单引号字符串
+			this.elements[0] = args;
+		}
 	}
+
 }
 
 
 
 //根据ID获取元素节点
 Base.prototype.getId = function(id){
-	this.elements.push(document.getElementById(id));
-	return this;
+	return document.getElementById(id);
 };
 
 //根据标签名获取元素节点
-Base.prototype.getTagName = function(tag){
-	var tags = document.getElementsByTagName(tag);
-	for(var i=0;i<tags.length;i++){
-		this.elements.push(tags[i]);
-	}
-
-	return this;
-};
-
-//根据CLASS获取结点数组
-Base.prototype.getClass = function(className,idName){
+Base.prototype.getTagName = function(tag,parentNode){
 	var node = null;
-	if(arguments.length == 2){
-		node = document.getElementById(idName);
+	var temps = [];
+	if(parentNode != undefined){
+		node = parentNode;
 	}else{
 		node = document;
 	}
 
+	var tags = node.getElementsByTagName(tag);
+	for(var i=0;i<tags.length;i++){
+		temps.push(tags[i]);
+	}
+
+	return temps;
+};
+
+//根据CLASS获取结点数组
+Base.prototype.getClass = function(className,parentNode){
+	var node = null;
+	var temps = [];
+	if(parentNode != undefined){
+		node = parentNode;
+	}else{
+		node = document;
+	}
 
 	var all = node.getElementsByTagName("*");
 	for(var i=0;i<all.length;i++){
 		if(all[i].className == className){
-			this.elements.push(all[i]);
+			temps.push(all[i]);
 		}
 	}
+
+	return temps;
+};
+
+
+//设置css选择器子节点
+Base.prototype.find = function(str){
+	var childElements = [];
+	for(var i = 0 ;i<this.elements.length;i++){
+		switch (str.charAt(0)){
+			case '#':
+				childElements.push(this.getId(str.substring(1)));
+				break;
+			case '.':
+
+
+				var temps = this.getClass(str.substring(1),this.elements[i]);
+				for(var j = 0;j < temps.length;j++){
+					childElements.push(temps[j]);
+				}
+
+
+				break;
+			default :
+				/*
+				var tags = this.elements[i].getElementsByTagName(str);
+				for(var j=0;j<tags.length;j++){
+					childElements.push(tags[j]);
+				}*/
+
+				var temps = this.getTagName(str,this.elements[i]);
+				for(var j = 0;j < temps.length;j++){
+					childElements.push(temps[j]);
+				}
+
+		}
+	}
+	 this.elements = childElements;
 	return this;
 };
 
