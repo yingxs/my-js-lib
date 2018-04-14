219552359a8d5f12256e71d63e7c2ce12b2a0704
王昊 <yingxs888@126.com>
2018-04-14 21:54:35 +0800

    feat (blog/):实现了年月日的动态注入

--- blog/index.js
+++ blog/index.js
@@ -212,6 +212,7 @@ $(function(){
 	//alert($('form').first().user.value);
 	//alert($('form').form('user').value('bbb'));
 
+
 	//用户名验证
 	$('form').form('user').bind('focus',function(){
 		$('#reg .info_user').css('display','block');
@@ -492,6 +493,57 @@ $(function(){
 		$(this).css('color','#666');
 
 	});
+
+	//年月日
+	var year = $('form').form('year');
+	var month = $('form').form('month');
+	var day = $('form').form('day');
+	var day30 = [4,6,9,11];
+	var day31 = [1,3,5,7,8,10,12];
+
+	//注入年
+	for(var i=1950;i<=2018 ;i++){
+		year.first().add(new Option(i,i),undefined);
+	}
+	//注入月
+	for(var i=1;i<=12 ;i++){
+		month.first().add(new Option(i,i),undefined);
+	}
+
+
+	//不确定的日期
+	var cur_day=0;
+
+	year.bind('change',select_day);
+	month.bind('change',select_day);
+
+	function select_day(){
+		if(year.value()!=0 && month.value() !=0 ){
+			//清理之前的注入
+			day.first().options.length=1;
+			//注入日
+			if(inArray(day31,parseInt(month.value()))){
+				cur_day=31;
+			}else if(inArray(day30,parseInt(month.value()))){
+				cur_day=30;
+			}else{
+				if((parseInt(year.value())%4 == 0 && parseInt(year.value()) % 100 != 0) || parseInt(year.value()) % 400 == 0 ){
+					cur_day=29;
+				}else{
+					cur_day=28;
+				}
+			}
+
+			for(var i = 1 ;i<=cur_day;i++){
+				day.first().add(new Option(i,i),undefined);
+			}
+
+		}else{
+			//清理之前的注入
+			day.first().options.length=1;
+		}
+	}
+
 
 
 	////test
