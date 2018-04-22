dc8376b7e765a76b5e46c4b2a405d124b204c7df
王昊 <yingxs888@126.com>
2018-04-22 23:03:40 +0800

    feat (tool.js):新增cookie方法

--- tool.js
+++ tool.js
@@ -349,4 +349,50 @@ function setInnerText(element,text){
 	}
 }
 
+//创建cookie
+function setCookie(name,value,expires,path,domain,secure){
+	var cookieText=encodeURIComponent(name)+'='+encodeURIComponent(value);
+	if(expires instanceof Date) {
+		cookieText+=';expires='+expires;
+	}
+	if(path){
+		cookieText+=';expires='+expires;
+	}
+	if(domain){
+		cookieText+=';domain='+domain;
+	}
+	if(secure){
+		cookieText+=';secure';
+	}
+	document.cookie=cookieText;
+}
+//获取cookie
+function getCookie(name){
+	var cookieName=encodeURIComponent(name)+'=';
+	var cookieStart=document.cookie.indexOf(cookieName);
+	var cookieValue=null;
+	if(cookieStart>-1){var cookieEnd = document.cookie.indexOf(';', cookieStart);
+		if (cookieEnd == -1) {
+			cookieEnd = document.cookie.length;
+		}
+		cookieValue= decodeURIComponent(document.cookie.substring(cookieStart + cookieName.length, cookieEnd));
+	}
+	return cookieValue;
+}
+//删除 cookie
+function unsetCookie(name) {
+	document.cookie = name +"= ; expires=" + new Date(0);
+}
+//失效天数，直接传一个天数即可
+function setCookieDate(day) {
+	if (typeof day== 'number' && day> 0){
+		var date = new Date();
+		date.setDate(date.getDate() + day);
+	} else {
+		throw new Error('传递的 day必须是一个天数，必须比 0大');
+	}
+	return date;
+}
+
+
 
